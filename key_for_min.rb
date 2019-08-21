d860249feb66642620e4d4fad4598a4e70f7995b
Katie Seggelke <katie.seggelke@gmail.com>
2019-08-21 17:14:13 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@ require "pry"
 
 def key_for_min_value(name_hash)
   smallest_value = 0 
-  smallest_key = ""
+  smallest_key = nil
   name_hash.each do |key, value|
     if smallest_value == 0 || value < smallest_value 
       smallest_value = value 
